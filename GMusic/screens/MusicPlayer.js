8b124370ac824dd55f013718973f7892a74ded86
Eduardo12viegas <eduardo.viegas@etec.sp.gov.br>
2024-09-16 11:39:25 -0300

    16/09/2024

--- GMusic/screens/MusicPlayer.js
+++ GMusic/screens/MusicPlayer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import {
     Dimensions,
+    FlatList,
     Image,
     SafeAreaView, 
     StyleSheet,
@@ -10,19 +11,79 @@ import {
     View } from 'react-native'
 import {Slider } from '@react-native-community/slider'    
 import {Ionicons} from '@expo/vector-icons'
+import songs from '../model/data'
 
 const { width, height }= Dimensions.get('window');
 
 const MusicPlayer = () => {
+
+    const rederSong = ({ item, index }) => {
+        return (
+            <View style={[styles.mainImageWraper]}>
+            <View style={[styles.imageWraper, styles.elevation]}>
+        <Image
+             source={ item.artwork }
+            style={styles.musicImage}
+            />
+            </View>
+         </View>
+        ) 
+    }
+
   return (
     <SafeAreaView style= {styles.container}>
       <View style={styles.main}>
-        <View style={[styles.imageWapper, styles.elevation]}>
-            <Image
-            source={require('../assets/img/gallo1.png')}
-            style={styles.musicImage}
-            />
+        
+        <FlatList
+          renderItem={renderSongs}
+          data={songs}
+          keyExtractor={ item => item.id }
+          horizontal
+          paddigEnabled
+          showsHorizontalScrollIndicator={false}
+          scrollEventThoug
+          
+          />
+
+        <View>
+            <Text style={[styles.songContent, styles.songTitle]}>
+                Titulo da Musica
+            </Text>
+            <Text style={[styles.songContent, styles.songArtist]}>
+                Autor da Musica
+            </Text>
         </View>
+
+        <View>
+            <Slider 
+              style={styles.progessBar}
+              Value={10}
+              minimumValue={0}
+              maximumValue={100}
+              thumbTintColor='#FFD369'
+              minimumTrackTintColor='#FFD369'
+              maximumTrackTintColor='FFF'
+              onSlidingComplete={() => {}}
+
+            />
+            <View style={styles.progessLevelDuration}>
+                <Text style={styles.progessLabelText}>00:00</Text>
+                <Text style={styles.progessLabelText}>00:00</Text>
+            </View>
+        </View>
+
+        <View style={styles.nusicControlsContainer}>
+            <TouchableOpacity onPress={() => {}}>
+                <Ionicons name='play-skip-back-outline' size={35} color="FFD369" />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => {}}>
+                <Ionicons name='pause-circle' size={75} color="FFD369" />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => {}}>
+                <Ionicons name='play-skip-forward-outline' size={35} color="FFD369" />
+            </TouchableOpacity>
+        </View>
+
       </View>
       <View style={styles.footer}>
         <View style={styles.iconWrapper}>
@@ -56,7 +117,56 @@ const styles = StyleSheet.create({
         flex: 1,
         aligItems: 'center',
         justifyContent: 'center',   
-        
+        marginBottom: 20,
+    },
+    mainImageWapper: {
+        width: width,
+        justifyContent: 'center',
+        aligItems: 'center',
+    },
+    imageWapper: {
+        width: 340,
+        height: 360,
+        marginVertical: 20,
+    },
+    musicImage: {
+        width: "100%",
+        height: "100%",
+        borderRadius: 15,
+    },
+    songContent: {
+        textAlign: 'center',
+        color: '#EEEEEE'
+    },
+    songTitle: {
+        fontSize: 16,
+        fontWeight: '600',
+    },
+    songArtist: {
+        fontSize: 16,
+        fontWeight: '300',
+    },
+    progessBar: {
+        width: 320,
+        height: 40,
+        marginTop: 20,
+        flexDirection: 'row',
+    },
+    progressLevelDuration: {
+        width: 340,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    progessLabelText: {
+        color: '#fff',
+        fontWeight: '500',
+    },
+    musicControlContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        width: '60%',
+        marginTop: 10,
     },
     footer: {
         width: width,
@@ -70,5 +180,13 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         width: '80%',
     },
-
+    elevation: {
+        elevation: 5,
+        shadowOffset: {
+        width: 5,
+        height: 5    
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 3.84
+    }
 })
